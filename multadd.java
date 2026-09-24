37ee208ffe12337a66323e4fbd3bac5b330747fa
arthurfan63-eng <arthurfan63@gmail.com>
2026-09-24 14:48:47 -0700

    Add files via upload

---
+++ multadd.java
@@ -0,0 +1,28 @@
+public class multadd{
+
+	public static double multadd(double a,double b,double c){
+	return a*b+c;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(multadd(Math.cos(Math.PI/4),0.5 , Math.sin(Math.PI/4)));
+		System.out.println(multadd(Math.log(10),1 , Math.log(20)));		
+	}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
